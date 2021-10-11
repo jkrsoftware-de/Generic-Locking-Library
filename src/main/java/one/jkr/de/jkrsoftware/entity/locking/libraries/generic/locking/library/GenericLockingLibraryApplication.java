5c64906739bef646dc092a9fb6780a46e12e5e08
Jeremy Krüger <jeremy.krueger@mercateo.com>
2021-10-11 13:09:19 +0200

    init with Spring Initializr.

---
+++ src/main/java/one/jkr/de/jkrsoftware/entity/locking/libraries/generic/locking/library/GenericLockingLibraryApplication.java
@@ -0,0 +1,13 @@
+package one.jkr.de.jkrsoftware.entity.locking.libraries.generic.locking.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericLockingLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericLockingLibraryApplication.class, args);
+	}
+
+}
