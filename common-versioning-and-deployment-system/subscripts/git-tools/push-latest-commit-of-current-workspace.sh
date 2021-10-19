5d1ef877033fca8f0e658e60251880ee5f830879
Jeremy Krüger (Jerry) <jeremy.krueger@jkrprojects.de>
2021-10-19 18:15:55 +0200

    Add some textual Informations (Readme, Contact-Infos, License) and the first Approach of the "Versioning- and Deployment-System".
    Signed-off-by: Jeremy Krüger (Jerry) <jeremy.krueger@jkrprojects.de>

---
+++ common-versioning-and-deployment-system/subscripts/git-tools/push-latest-commit-of-current-workspace.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+###### 🌏 ###### 💬 ######
+# All my public-accessible Software-Content aren't underlying any Software-License. It's Free-Software.
+# You can use my Code as you want — my only Wish is: mark me as Original-Creator of your Derivation. But you mustn't. :)
+#
+# My Intention of creating/publishing Free-Software is to help our Public Society.
+# In this particular Case our newly-created "Computer-World". I mean everything regarding complex IT-Systems.
+#
+# made for jkrsoftware.de as Versioning- and Deployment-System.
+# made with ❤ by Jeremy Krüger (jkr.one). 😊
+###### 🌏 ###### 💬 ######
+
+readonly BRANCH_TO_COMMIT=${1}
+
+if [[ -z ${BRANCH_TO_COMMIT} ]]; then
+  echo "Can't push the latest Commit (of the current Workspace) to the Remote-Repository, cause you don't provided a Remote-Branch.";
+  exit 1
+fi
+
+readonly LAST_COMMIT_ID="$(git rev-parse --verify HEAD)"
+
+echo "${LOG_PREFIX}Push the latest Commit (of the current Workspace) to the Git-Repository."
+git checkout -b ${BRANCH_TO_COMMIT}
+git push origin ${LAST_COMMIT_ID}:${BRANCH_TO_COMMIT} --tags
