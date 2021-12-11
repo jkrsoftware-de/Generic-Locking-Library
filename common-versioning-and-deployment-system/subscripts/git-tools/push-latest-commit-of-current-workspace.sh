18f30d280cfb8d92030e22959018ef853fe201ef
Jeremy Krüger <jeremy.krueger@mercateo.com>
2021-12-11 22:00:59 +0100

    just commit. <3
    Signed-off-by: Jeremy Krüger <jeremy.krueger@mercateo.com>

--- common-versioning-and-deployment-system/subscripts/git-tools/push-latest-commit-of-current-workspace.sh
+++ common-versioning-and-deployment-system/subscripts/git-tools/push-latest-commit-of-current-workspace.sh
@@ -21,5 +21,4 @@ readonly LAST_COMMIT_ID="$(git rev-parse --verify HEAD)"
 
 echo "${LOG_PREFIX}Push the latest Commit (of the current Workspace) to the Git-Repository."
 git checkout -b ${BRANCH_TO_COMMIT}
-git push --tags
-git push origin ${LAST_COMMIT_ID}:${BRANCH_TO_COMMIT} --tags
+git push origin --tags
